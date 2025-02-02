1c98cf5d68268c8a28e6fbacca37f43a79bc2e1d
dimaboronin2004 <112697344+dimaboronin2004@users.noreply.github.com>
2025-02-03 00:47:51 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        InternetShop shop = InternetShop.INTERNET_SHOP;
+        shop.fillShop(); //Заполнение магазина
+        shop.baseCycle(); //Основной рабочий цикл магазина
+    }
+}
